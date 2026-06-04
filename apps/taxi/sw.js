b2a57c363a8c8bd65bb34e3033bfd13e5ee25c7b
tejinder1business-netizen <tejinder1business@gmail.com>
2026-06-03 22:21:43 -0700

    Create sw.js

---
+++ apps/taxi/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'taxi-cache-v1';
+const ASSETS = [
+  'index.html',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
