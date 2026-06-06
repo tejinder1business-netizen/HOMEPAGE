34f5cbbc6618f1b776cb569af93f0c9e20d331ff
tejinder1business-netizen <tejinder1business@gmail.com>
2026-06-06 12:27:24 -0700

    Update sw.js

--- apps/taxi/sw.js
+++ apps/taxi/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'taxi-cache-v1';
+const CACHE_NAME = 'taxi-cache-v2';
 const ASSETS = [
   'index.html',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
@@ -7,11 +7,28 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Forces the phone to install the update immediately
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
     })
   );
+});
+
+// THIS IS THE NEW VAULT CLEANER
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // Deletes the old v1 memory
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim(); // Takes control of the app immediately
 });
 
 self.addEventListener('fetch', (e) => {
